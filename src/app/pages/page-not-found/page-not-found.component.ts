0627c0ab12ab0627ddfa339989374dca99fab443
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-01-08 01:50:19 +0700

    fix(page-not-found.component.ts): update import 404 svg

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { PageNotFoundSVGComponent } from '~/components/svg/404/page-not-found-svg.component';
+import { PageNotFoundSVGComponent } from '~/components/svg/notfoundSVG/page-not-found-svg.component';
 
 @Component({
   selector: 'app-page-not-found',
